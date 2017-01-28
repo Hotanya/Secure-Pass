ba5f72fa67aaad5d0b5b5f33002acf664d966f75
Hotanya <hotanya.r@gmail.com>
2017-01-28 22:12:27 +1300

    New project coz old one was screwing up

--- SecurePass/SecurePass/App.cs
+++ SecurePass/SecurePass/App.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 using Xamarin.Forms;
 
@@ -28,9 +29,9 @@ namespace SecurePass
             };
 
             MainPage = new NavigationPage(content);
-        }
+        }       
 
-        protected override void OnStart()
+            protected override void OnStart()
         {
             // Handle when your app starts
         }
@@ -43,6 +44,22 @@ namespace SecurePass
         protected override void OnResume()
         {
             // Handle when your app resumes
+        }
+    }
+
+    
+    public class Main
+    {
+        // need to add a button or something which will do this when clicked
+        public static void GeneratePass()
+        {
+            string[] lower = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            string[] upper = new string[26] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            int[] number = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            string[] special = new string[13] { "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+" };
+
+            foreach (string i in lower)
+                Debug.WriteLine(i);
         }
     }
 }

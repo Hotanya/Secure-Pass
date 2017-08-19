6a64daed44681a65b0c9fbcb768bc57bef297547
Hotanya <hotanya.r@gmail.com>
2017-08-19 18:47:56 +1200

    Generates a 8 character password. But repeats the same random character for each type (eg. AAbb11--)

--- SecurePass/SecurePass/SecurePass.xaml.cs
+++ SecurePass/SecurePass/SecurePass.xaml.cs
@@ -21,9 +21,9 @@ namespace SecurePass
         void OnButtonClicked(object sender, EventArgs args)
         {
             var lower = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-            string[] upper = new string[26] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-            int[] number = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            string[] special = new string[13] { "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+" };
+            var upper = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            var number = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var special = new List<string> { "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+" };
 
             //Starting the Random
             Random r = new Random();
@@ -32,14 +32,35 @@ namespace SecurePass
             int countLower = r.Next(lower.Count); 
             foreach (string i in lower)
             {
-                valueLabel.Text = lower[countLower];
-                
+                //valueLabel.Text = lower[countLower];
+                var l = lower[countLower];
             }
 
             //for upper
-            int countUpper = r.Next(lower.Count);
+            int countUpper = r.Next(upper.Count);
             foreach (string i in upper)
-                valueLabel.Text = upper[countUpper];
+            {
+                //valueLabel.Text = upper[countUpper];
+                var u = upper[countUpper];
+            }
+
+            //for number
+            int countNumber = r.Next(number.Count);
+            foreach (int i in number)
+            {
+                //valueLabel.Text = number[countNumber];
+                var n = number[countNumber];
+            }
+
+            //for special
+            int countSpecial = r.Next(special.Count);
+            foreach (string i in special)
+            {
+                //valueLabel.Text = special[countSpecial];
+                var s = special[countSpecial];
+            }
+
+            valueLabel.Text = "Your password is: " + lower[countLower] + lower[countLower] + upper[countUpper] + upper[countUpper] + number[countNumber] + number[countNumber] + special[countSpecial] + special[countSpecial];
         }
     }
 }

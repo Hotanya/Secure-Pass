d7e184841e4d23e7596671b2b995e59f4cf1048c
Hotanya <hotanya.r@gmail.com>
2017-08-19 17:40:55 +1200

    added button on page, copied code from App.cs to xaml.cs page

--- SecurePass/SecurePass/SecurePass.xaml.cs
+++ SecurePass/SecurePass/SecurePass.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,17 @@ namespace SecurePass
         public SecurePass()
         {
             InitializeComponent();
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            string[] lower = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            string[] upper = new string[26] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            int[] number = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            string[] special = new string[13] { "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+" };
+
+            foreach (string i in lower)
+                Debug.WriteLine(i);
         }
     }
 }

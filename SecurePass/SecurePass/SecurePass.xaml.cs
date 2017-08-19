afff2f8c2df065cf84a4e4d5c7bfea43d1548c82
Hotanya <hotanya.r@gmail.com>
2017-08-19 18:07:41 +1200

    successful output of lower on button press

--- SecurePass/SecurePass/SecurePass.xaml.cs
+++ SecurePass/SecurePass/SecurePass.xaml.cs
@@ -20,13 +20,26 @@ namespace SecurePass
 
         void OnButtonClicked(object sender, EventArgs args)
         {
-            string[] lower = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            var lower = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
             string[] upper = new string[26] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
             int[] number = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             string[] special = new string[13] { "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+" };
 
+            //Starting the Random
+            Random r = new Random();
+            
+            //for lower
+            int countLower = r.Next(lower.Count); 
             foreach (string i in lower)
-                Debug.WriteLine(i);
+            {
+                valueLabel.Text = lower[countLower];
+                
+            }
+
+            //for upper
+            int countUpper = r.Next(lower.Count);
+            foreach (string i in upper)
+                valueLabel.Text = upper[countUpper];
         }
     }
 }

e2904e6df5cbfd2110088020329c1c29d910147d
Hotanya <hotanya.r@gmail.com>
2017-08-22 17:11:52 +1200

    added all the crud code

---
+++ SecurePass/SecurePass.Droid/DatabaseConnection_Android.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using SecurePass.Droid;
+using System.IO;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DatabaseConnection_Android))]
+namespace SecurePass.Droid
+{
+   public class DatabaseConnection_Android
+    {
+        public SQLiteConnection DbConnection()
+        {
+            var dbName = "UserDb.db3";
+            var path = Path.Combine(System.Environment.
+              GetFolderPath(System.Environment.
+              SpecialFolder.Personal), dbName);
+            return new SQLiteConnection(path);
+        }
+    }
+}
---
+++ SecurePass/SecurePass/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using System.ComponentModel;
+
+namespace SecurePass
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}

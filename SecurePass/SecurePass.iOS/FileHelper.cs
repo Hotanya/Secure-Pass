eb15e29bd275c5d5468c8fe7951df6d7f6b78b71
Hotanya <hotanya.r@gmail.com>
2017-08-25 09:33:15 +1200

    cleaned up code and added ios db stuff

---
+++ SecurePass/SecurePass.iOS/FileHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using SecurePass.iOS;
+using SQLite;
+using System.IO;
+
+[assembly: Xamarin.Forms.Dependency(typeof(FileHelper))]
+namespace SecurePass.iOS
+{
+    class FileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string libFolder = Path.Combine(docFolder, "..", "Library", "Databases");
+
+            if (!Directory.Exists(libFolder))
+            {
+                Directory.CreateDirectory(libFolder);
+            }
+
+            return Path.Combine(libFolder, filename);
+        }
+    }
+}

31c495858fa46d5b3a7c2aae1b6f124015c45e9a
Hotanya <hotanya.r@gmail.com>
2017-08-27 22:09:48 +1200

    Added a check to see if Account Name or Password is empty

--- SecurePass/SecurePass/Pages/DetailsPage.xaml.cs
+++ SecurePass/SecurePass/Pages/DetailsPage.xaml.cs
@@ -13,6 +13,8 @@ namespace SecurePass.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailsPage : ContentPage
     {
+        string nameEntry { get; set; }
+        string passEntry { get; set; }
         public DetailsPage()
         {
             
@@ -31,18 +33,17 @@ namespace SecurePass.Pages
 
             var saveButton = new Button { Text = "Save" };
             saveButton.Clicked += async (sender, e) =>
-            {
-                if (nameEntry.Text.Length == 0)
                 {
-                    await DisplayAlert("Oops", "Please enter an account name", "OK");
-                }
-                else
-                {
-                    var application = (User)BindingContext;
-                    await App.Database.SaveApplicationAsync(application);
-                    await Navigation.PopAsync();
-                }
-                
+                    if (nameEntry.Text == null || passEntry.Text == null)
+                    {
+                        await DisplayAlert("Oops", "Please enter an account name", "OK");
+                    }
+                    else
+                    {
+                        var application = (User)BindingContext;
+                        await App.Database.SaveApplicationAsync(application);
+                        await Navigation.PopAsync();
+                    }
             };
 
             var deleteButton = new Button { Text = "Delete" };
@@ -51,7 +52,7 @@ namespace SecurePass.Pages
                 {
                     var result =
                       await DisplayAlert("Confirmation", "Are you sure? This cannot be undone",
-                      "OK", "Cancel");
+                      "Yes", "Cancel");
                     if (result == true)
                     {
                         var application = (User)BindingContext;
@@ -89,6 +90,9 @@ namespace SecurePass.Pages
                 }
 
             };
+
+
+
             //why this no work??
             if (doneSwitch.IsToggled) {
                 var toggle = passEntry.IsEnabled == true;
@@ -99,20 +103,18 @@ namespace SecurePass.Pages
                 return;
             }
         }
-        string nameEntry { get; set; }
-        string passEntry { get; set; }
-
-
 
 
         async void OnSaveClicked(object sender, EventArgs e)
         {
-            validate(nameEntry, passEntry);
+            //validate(nameEntry, passEntry);
             var application = (User)BindingContext;
             await App.Database.SaveApplicationAsync(application);
             await Navigation.PopAsync();
            
         }
+
+        
 
         private async void OnDeleteClicked(object sender, EventArgs e)
         {
@@ -128,18 +130,5 @@ namespace SecurePass.Pages
             await Navigation.PopAsync();
         }
 
-        async void validate(string accountName, string password)
-        {
-            if (!string.IsNullOrWhiteSpace(accountName) && !string.IsNullOrWhiteSpace(password))
-            {
-                User user = new User
-                {
-                    AccountName = accountName
-
-                };
-                await DisplayAlert("Oops", "Please enter an account name", "OK");
-
-            }
-        }
     }
 }
